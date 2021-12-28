43e9d30058461a3ca5b0e7a67e41ec5f448e4036
Daniel Nathan <danielnatham@gmail.com>
2021-12-28 09:28:03 -0300

    [feat] Model Factory and Database seed

---
+++ database/factories/LinkFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Link;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class LinkFactory extends Factory
+{
+
+    protected $model = Link::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'url' => $this->faker->url(),
+            'slug' => Str::random(6),
+        ];
+    }
+}
