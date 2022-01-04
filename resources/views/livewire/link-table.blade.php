8504c406ca9fa5dcd84409e75b27fb61f464e3f1
Daniel Nathan <danielnatham@gmail.com>
2022-01-04 13:30:42 -0300

    [Fix] Ajustando View Tabela de Links e GitIgnore

--- resources/views/livewire/link-table.blade.php
+++ resources/views/livewire/link-table.blade.php
@@ -17,6 +17,9 @@
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Criado em
                             </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Expira em
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
